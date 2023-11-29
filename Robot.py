0f443f53df8da3a7a4ef55e261fa5547cef43971
mohammed <md-hn-50@hotmail.com>
2023-11-29 17:22:07 +0000

    fixed the transition model and the get_all_states() method

--- Robot.py
+++ Robot.py
@@ -13,9 +13,10 @@ class RRSPState(pomdp_py.State):
         self.oScissor = oScissor
 
     def __str__(self):
-        return "My points:\nrocks: " + str(self.myPoints[0]) + ", papers: " + str(
-            self.myPoints[1]) + ", scissors: " + str(self.myPoints[2]) + "Opponent's points:\nrocks: " + str(
-            self.oPoints[0]) + ", papers: " + str(self.oPoints[1]) + ", scissors: " + str(self.oPoints[2])
+        return ("My points:\nrocks: " + str(self.myPoints[0]) + ", papers: " + str(
+        self.myPoints[1]) + ", scissors: " + str(self.myPoints[2]) + "\nOpponent's points:\nrocks: " + str(
+        self.oPoints[0]) + ", papers: " + str(self.oPoints[1]) + ", scissors: " + str(self.oPoints[2]) + "\nHand: " + str(self.cards) + "\nObserved rocks: " + str(
+        self.oRock)) + "\nObserved papers: " + str(self.oPaper) + "\nObserved scissors: " + str(self.oScissor)
     def __hash__(self):
         return hash(self.__str__())
     def __eq__(self, other):
@@ -128,95 +129,210 @@ class ObservationModel(pomdp_py.ObservationModel):
 
 
 standard = ['r','p','s'] #Put somewhere
-global ss
-ss = RRSPState([0,0,0], [0,0,0], ['-','-','-','-','-'], 0,0,0)
+
 
 class TransitionModel(pomdp_py.TransitionModel):
+    def __init__(self, cur_state):
+        self.cur_state = cur_state
     def probability(self, start_state, end_state, action):
-
-        base = 1/3 #at start prime_prob MUST be roughly == this
-
-        #Can the opponent make it end_state?
-
-        #First find the opponents move required to get to the end_state
-        if (sum(end_state.myPoints) > sum(start_state.myPoints)): # player wins in end_state, opponent loses
-            opp_action = (standard.index(action.name[5]) + 1) % 3
-        elif (sum(end_state.oPoints) > sum(start_state.oPoints)): # opponent wins in end_state, player loses
-            opp_action = (standard.index(action.name[5]) + 2) % 3
+        if end_state.myPoints[0] - start_state.myPoints[0] == 1 and action.name != "play_rock":
+            return 0
+        elif end_state.myPoints[1] - start_state.myPoints[1] == 1 and action.name != "play_paper":
+            return 0
+        elif end_state.myPoints[2] - start_state.myPoints[2] == 1 and action.name != "play_scissor":
+            return 0
+        elif end_state.oPoints[0] - start_state.oPoints[0] == 1 and action.name != "play_scissor":
+            return 0
+        elif end_state.oPoints[1] - start_state.oPoints[1] == 1 and action.name != "play_rock":
+            return 0
+        elif end_state.oPoints[2] - start_state.oPoints[2] == 1 and action.name != "play_paper":
+            return 0
         else:
-            opp_action = standard.index(action.name[5])
+            base = 1/3 #at start prime_prob MUST be roughly == this
 
-        #Secondly, calculate the probability that the opponent wants to play that card P(plays x):
-        # (1) Higher Probability of playing cards that have a chance of getting them a big win (Offense)
-        # (2) Higher Probability of playing cards that block our big win (defense)
-        # (3) To play the card, they need it in hand
+            #Can the opponent make it end_state?
 
-        #(1)
-        #See how close the opponent is to achieving the win condition of "winning with 1 of each type"
-        if (start_state.oPoints[(opp_action+1)%3] >= 1) or (start_state.oPoints[(opp_action+2)%3] >= 1):
-            spread_win_closeness = 1/3
-            if (start_state.oPoints[(opp_action+1)%3] >= 1) and (start_state.oPoints[(opp_action+2)%3] >= 1):
-                spread_win_closeness = 2/3
-        else:
-            spread_win_closeness = 0
+            #First find the opponents move required to get to the end_state
+            if (sum(end_state.myPoints) > sum(start_state.myPoints)): # player wins in end_state, opponent loses
+                opp_action = (standard.index(action.name[5]) + 1) % 3
+            elif (sum(end_state.oPoints) > sum(start_state.oPoints)): # opponent wins in end_state, player loses
+                opp_action = (standard.index(action.name[5]) + 2) % 3
+            else:
+                opp_action = standard.index(action.name[5])
 
-        #Extract the win condition the opponent is closer to in the form of a fraction of "closeness"
-        opp_win_closeness = max((start_state.oPoints[opp_action] / 3), spread_win_closeness)
+            #Secondly, calculate the probability that the opponent wants to play that card P(plays x):
+            # (1) Higher Probability of playing cards that have a chance of getting them a big win (Offense)
+            # (2) Higher Probability of playing cards that block our big win (defense)
+            # (3) To play the card, they need it in hand
 
-        #(2)
-        #Get highest point win type
-        highest = max(start_state.myPoints)
-        highest_types = []
-        for i in range(3):
-            if start_state.myPoints[i] == highest:
-                highest_types.append(i)
+            #(1)
+            #See how close the opponent is to achieving the win condition of "winning with 1 of each type"
+            if (start_state.oPoints[(opp_action+1)%3] >= 1) or (start_state.oPoints[(opp_action+2)%3] >= 1):
+                spread_win_closeness = 1/3
+                if (start_state.oPoints[(opp_action+1)%3] >= 1) and (start_state.oPoints[(opp_action+2)%3] >= 1):
+                    spread_win_closeness = 2/3
+            else:
+                spread_win_closeness = 0
 
-        defense_effect = 0
-        if len(highest_types) != 3:
-            for item in highest_types:
-                if (item+1)%3 == opp_action:
-                    defense_effect += 0.6
-                elif item == opp_action:
-                    defense_effect += 0.4
-                else:
-                    defense_effect += 0.1                               # Maybe want negative value?
-            defense_effect = defense_effect / len(highest_types)
+            #Extract the win condition the opponent is closer to in the form of a fraction of "closeness"
+            opp_win_closeness = max((start_state.oPoints[opp_action] / 3), spread_win_closeness)
 
-        #Calculate the probability of the opponent playing that card.
+            #(2)
+            #Get highest point win type
+            highest = max(start_state.myPoints)
+            highest_types = []
+            for i in range(3):
+                if start_state.myPoints[i] == highest:
+                    highest_types.append(i)
 
-        play_x_prob = base + opp_win_closeness + defense_effect         # Probably not how math works
+            defense_effect = 0
+            if len(highest_types) != 3:
+                for item in highest_types:
+                    if (item+1)%3 == opp_action:
+                        defense_effect += 0.6
+                    elif item == opp_action:
+                        defense_effect += 0.4
+                    else:
+                        defense_effect += 0.1                               # Maybe want negative value?
+                defense_effect = defense_effect / len(highest_types)
 
-        #Thirdly, calculate the probability opponent plays that card given they've picked it up.
-        #P(plays x | card_in_hand) = P(card_in_hand | plays x) * P(plays x) / P(card_in_hand)
+            #Calculate the probability of the opponent playing that card.
 
-        if standard[opp_action] == 'r':
-            prime_prob = defense_effect * play_x_prob/ (10 - start_state.oRock / (30 - (start_state.oRock + start_state.oPaper + start_state.oScissor))) #(3)
-        if standard[opp_action] == 'p':
-            prime_prob = defense_effect * play_x_prob/ (10 - start_state.oPaper / (30 - (start_state.oRock + start_state.oPaper + start_state.oScissor))) #(3)
-        else:
-            prime_prob = defense_effect * play_x_prob/ (10 - start_state.oScissor / (30 - (start_state.oRock + start_state.oPaper + start_state.oScissor)))
+            play_x_prob = base + opp_win_closeness + defense_effect         # Probably not how math works
 
-        return prime_prob
+            #Thirdly, calculate the probability opponent plays that card given they've picked it up.
+            #P(plays x | card_in_hand) = P(card_in_hand | plays x) * P(plays x) / P(card_in_hand)
+
+            if standard[opp_action] == 'r':
+                prime_prob = defense_effect * play_x_prob/ (10 - start_state.oRock / (30 - (start_state.oRock + start_state.oPaper + start_state.oScissor))) #(3)
+            if standard[opp_action] == 'p':
+                prime_prob = defense_effect * play_x_prob/ (10 - start_state.oPaper / (30 - (start_state.oRock + start_state.oPaper + start_state.oScissor))) #(3)
+            else:
+                prime_prob = defense_effect * play_x_prob/ (10 - start_state.oScissor / (30 - (start_state.oRock + start_state.oPaper + start_state.oScissor)))
+
+            return prime_prob
 
     def sample(self, state, action):
-        probabilities = [self.probability(ss,state,action) for state in self.get_all_states()]
+        probabilities = [self.probability(self.cur_state,state,action) for state in self.get_all_states()]
         remaining = 1 - sum(probabilities)
 
         for i in range(len(probabilities)-1):
             probabilities[i] += remaining/len(probabilities)
 
         return random.choices(self.get_all_states(), probabilities)[0]
+
+    def replace(self, cards, init, end):
+        new_cards = cards.copy()
+        index = cards.index(init)
+        new_cards[index] = end
+        return new_cards
+
     def get_all_states(self):
-
-        states = [ss]         # all draws
-
-        states.append(RRSPState([ss.myPoints[0] + 1, ss.myPoints[1], ss.myPoints[2]], ss.oPoints, ss.cards, ss.oRock, ss.oPaper, ss.oScissor))    #all wins
-        states.append(RRSPState([ss.myPoints[0], ss.myPoints[1] + 1, ss.myPoints[2]], ss.oPoints, ss.cards, ss.oRock, ss.oPaper, ss.oScissor))
-        states.append(RRSPState([ss.myPoints[0], ss.myPoints[1], ss.myPoints[2] + 1], ss.oPoints, ss.cards, ss.oRock, ss.oPaper, ss.oScissor))
-        states.append(RRSPState(ss.myPoints, [ss.oPoints[0] + 1, ss.oPoints[1], ss.oPoints[2]], ss.cards, ss.oRock, ss.oPaper, ss.oScissor))       #all losses
-        states.append(RRSPState(ss.myPoints, [ss.oPoints[0], ss.oPoints[1] + 1, ss.oPoints[2]], ss.cards, ss.oRock, ss.oPaper, ss.oScissor))
-        states.append(RRSPState(ss.myPoints, [ss.oPoints[0], ss.oPoints[1], ss.oPoints[2] + 1], ss.cards, ss.oRock, ss.oPaper, ss.oScissor))
-
+        ss = self.cur_state
+        states = []         # all draws
+        #played rock
+        if("rock" in ss.cards):
+            # draw
+            states.append(RRSPState(ss.myPoints, ss.oPoints, self.replace(ss.cards, "rock", "rock"), ss.oRock + 2, ss.oPaper, ss.oScissor))
+            states.append(
+                RRSPState(ss.myPoints, ss.oPoints, self.replace(ss.cards, "rock", "paper"), ss.oRock + 1, ss.oPaper+1,
+                          ss.oScissor))
+            states.append(
+                RRSPState(ss.myPoints, ss.oPoints, self.replace(ss.cards, "rock", "scissor"), ss.oRock + 1, ss.oPaper,
+                          ss.oScissor+1))
+            # won
+            states.append(
+                RRSPState([ss.myPoints[0]+1, ss.myPoints[1], ss.myPoints[2]], ss.oPoints, self.replace(ss.cards, "rock", "rock"), ss.oRock + 1, ss.oPaper,
+                          ss.oScissor+1))
+            states.append(
+                RRSPState([ss.myPoints[0] + 1, ss.myPoints[1], ss.myPoints[2]], ss.oPoints,
+                          self.replace(ss.cards, "rock", "paper"), ss.oRock, ss.oPaper+1,
+                          ss.oScissor + 1))
+            states.append(
+                RRSPState([ss.myPoints[0] + 1, ss.myPoints[1], ss.myPoints[2]], ss.oPoints,
+                          self.replace(ss.cards, "rock", "scissor"), ss.oRock, ss.oPaper,
+                          ss.oScissor + 2))
+            # lost
+            states.append(
+                RRSPState(ss.myPoints, [ss.oPoints[0], ss.oPoints[1]+1, ss.oPoints[2]],
+                          self.replace(ss.cards, "rock", "rock"), ss.oRock + 1, ss.oPaper+1,
+                          ss.oScissor))
+            states.append(
+                RRSPState(ss.myPoints, [ss.oPoints[0], ss.oPoints[1] + 1, ss.oPoints[2]],
+                          self.replace(ss.cards, "rock", "paper"), ss.oRock, ss.oPaper + 2,
+                          ss.oScissor))
+            states.append(
+                RRSPState(ss.myPoints, [ss.oPoints[0], ss.oPoints[1] + 1, ss.oPoints[2]],
+                          self.replace(ss.cards, "rock", "scissor"), ss.oRock, ss.oPaper + 1,
+                          ss.oScissor+1))
+        if("paper" in ss.cards):
+            # draw
+            states.append(RRSPState(ss.myPoints, ss.oPoints, self.replace(ss.cards, "paper", "rock"), ss.oRock + 1, ss.oPaper+1, ss.oScissor))
+            states.append(
+                RRSPState(ss.myPoints, ss.oPoints, self.replace(ss.cards, "paper", "paper"), ss.oRock, ss.oPaper+2,
+                          ss.oScissor))
+            states.append(
+                RRSPState(ss.myPoints, ss.oPoints, self.replace(ss.cards, "paper", "scissor"), ss.oRock, ss.oPaper+1,
+                          ss.oScissor+1))
+            # won
+            states.append(
+                RRSPState([ss.myPoints[0], ss.myPoints[1]+1, ss.myPoints[2]], ss.oPoints, self.replace(ss.cards, "paper", "rock"), ss.oRock + 2, ss.oPaper,
+                          ss.oScissor))
+            states.append(
+                RRSPState([ss.myPoints[0], ss.myPoints[1]+1, ss.myPoints[2]], ss.oPoints,
+                          self.replace(ss.cards, "paper", "paper"), ss.oRock+1, ss.oPaper+1,
+                          ss.oScissor))
+            states.append(
+                RRSPState([ss.myPoints[0], ss.myPoints[1]+1, ss.myPoints[2]], ss.oPoints,
+                          self.replace(ss.cards, "paper", "scissor"), ss.oRock+1, ss.oPaper,
+                          ss.oScissor+1))
+            # lost
+            states.append(
+                RRSPState(ss.myPoints, [ss.oPoints[0], ss.oPoints[1], ss.oPoints[2]+1],
+                          self.replace(ss.cards, "paper", "rock"), ss.oRock + 1, ss.oPaper,
+                          ss.oScissor+1))
+            states.append(
+                RRSPState(ss.myPoints, [ss.oPoints[0], ss.oPoints[1], ss.oPoints[2]+1],
+                          self.replace(ss.cards, "paper", "paper"), ss.oRock, ss.oPaper + 1,
+                          ss.oScissor+1))
+            states.append(
+                RRSPState(ss.myPoints, [ss.oPoints[0], ss.oPoints[1], ss.oPoints[2]+1],
+                          self.replace(ss.cards, "paper", "scissor"), ss.oRock, ss.oPaper,
+                          ss.oScissor+2))
+        if("scissor" in ss.cards):
+            # draw
+            states.append(RRSPState(ss.myPoints, ss.oPoints, self.replace(ss.cards, "scissor", "rock"), ss.oRock + 1, ss.oPaper, ss.oScissor+1))
+            states.append(
+                RRSPState(ss.myPoints, ss.oPoints, self.replace(ss.cards, "scissor", "paper"), ss.oRock, ss.oPaper+1,
+                          ss.oScissor+1))
+            states.append(
+                RRSPState(ss.myPoints, ss.oPoints, self.replace(ss.cards, "scissor", "scissor"), ss.oRock, ss.oPaper,
+                          ss.oScissor+2))
+            # won
+            states.append(
+                RRSPState([ss.myPoints[0], ss.myPoints[1], ss.myPoints[2]+1], ss.oPoints, self.replace(ss.cards, "scissor", "rock"), ss.oRock+1, ss.oPaper+1,
+                          ss.oScissor))
+            states.append(
+                RRSPState([ss.myPoints[0], ss.myPoints[1], ss.myPoints[2]+1], ss.oPoints,
+                          self.replace(ss.cards, "scissor", "paper"), ss.oRock, ss.oPaper+2,
+                          ss.oScissor))
+            states.append(
+                RRSPState([ss.myPoints[0], ss.myPoints[1], ss.myPoints[2]+1], ss.oPoints,
+                          self.replace(ss.cards, "scissor", "scissor"), ss.oRock, ss.oPaper+1,
+                          ss.oScissor + 1))
+            # lost
+            states.append(
+                RRSPState(ss.myPoints, [ss.oPoints[0]+1, ss.oPoints[1], ss.oPoints[2]],
+                          self.replace(ss.cards, "scissor", "rock"), ss.oRock + 2, ss.oPaper,
+                          ss.oScissor))
+            states.append(
+                RRSPState(ss.myPoints, [ss.oPoints[0]+1, ss.oPoints[1], ss.oPoints[2]],
+                          self.replace(ss.cards, "scissor", "paper"), ss.oRock+1, ss.oPaper + 1,
+                          ss.oScissor))
+            states.append(
+                RRSPState(ss.myPoints, [ss.oPoints[0]+1, ss.oPoints[1], ss.oPoints[2]],
+                          self.replace(ss.cards, "scissor", "scissor"), ss.oRock+1, ss.oPaper,
+                          ss.oScissor+1))
         return states
 
 class RewardModel(pomdp_py.RewardModel):
@@ -373,12 +489,12 @@ class RRSPProblem(pomdp_py.POMDP):
         #defining the agent
         agent = pomdp_py.Agent(init_belief,
                                PolicyModel(),
-                               TransitionModel(),
+                               TransitionModel(init_true_state),
                                ObservationModel(init_true_state),
                                RewardModel())        
         
         #defining the environment
-        env = pomdp_py.Environment(init_true_state, TransitionModel(), RewardModel())
+        env = pomdp_py.Environment(init_true_state, TransitionModel(init_true_state), RewardModel())
         
         super().__init__(agent, env, name = "RRSPPRroblem")
     
@@ -399,12 +515,12 @@ def test_planner(rrsp_problem, planner, debug_tree = False):
     print("Belief State:", rrsp_problem.agent.belief)
     print("Action:", action.name)
 
-    reward = max([rrsp_problem.env.reward_model.sample(rrsp_problem.env.state, action, next_state) for next_state in rrsp_problem.env.transition_model.get_all_states()])
+    reward = rrsp_problem.env.state_transition(action, execute=True)
     print("Reward:", reward)
 
     #real_observation = rrsp_problem.env.observation_model.sample(rrsp_problem.env.state, action)
     real_observation = random.choice(rrsp_problem.agent.observation_model.get_all_observations())
-    print("Observation:", real_observation.name)
+    print("Observation:", rrsp_problem.env.state)
     rrsp_problem.agent.update_history(action, real_observation)
 
     # Save the state
@@ -432,7 +548,7 @@ def initialize_state(end_state = None):
         rocks_count = 2 #random.randint(0, total_cards)
         papers_count = 2#random.randint(0, total_cards - rocks_count)
         scissors_count = 1#total_cards - rocks_count - papers_count
-        return RRSPState([0,0,0], [0,0,0], [rocks_count, papers_count, scissors_count], rocks_count, papers_count, scissors_count)
+        return RRSPState([0,0,0], [0,0,0], ["rock", "rock", "paper", "paper", "scissor"], rocks_count, papers_count, scissors_count)
     else:
         # Use end state to initialize the new state
         return RRSPState(end_state.myPoints, end_state.oPoints, end_state.cards, end_state.oRock, end_state.oPaper, end_state.oScissor)
